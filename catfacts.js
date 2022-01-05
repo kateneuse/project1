5039a07310b2698c13e2a4bc72b1d29dc3f9d322
Kate Neuse <kneu4581@gmail.com>
2022-01-05 14:20:39 -0600

    updated catfacts.js

--- catfacts.js
+++ catfacts.js
@@ -6,5 +6,30 @@ fetch ("https://cat-fact.herokuapp.com/facts",{
 })
 .then (response => response.json())
 .then (function(data){
-    console.log(data)
+    console.log("we got a response")
 })
+
+
+const button = document.getElementById("button");
+
+button.addEventListener('click', function() {
+
+
+function getFact() {
+  fetch('https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=1')
+  .then(response => response.text())
+  .then(data => {
+    let fact = JSON.parse(data).text
+    console.log(fact)
+    var p = document.createElement("p");
+       var node = document.createTextNode(fact);
+       p.appendChild(node);
+
+       let facts = document.querySelector(".facts");
+       
+      })
+  }
+getFact()
+})
+
+
