9a6dc07a1474b077803adc97b9f095a28c1165a6
Kate Neuse <kneu4581@gmail.com>
2022-01-06 15:42:30 -0600

    generator javascript

--- catfacts.js
+++ catfacts.js
@@ -10,31 +10,11 @@ fetch ("https://cat-fact.herokuapp.com/facts",{
 })
 
 
-const button = document.getElementById("fun-fact-btn");
-
-button.addEventListener('click', function() {
-
-
-function getFact() {
-  fetch('https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=1')
-  .then(response => response.text())
-  .then(data => {
-    let fact = JSON.parse(data).text
-    console.log(fact)
-    var displayEl = document.getElementById("display-fun-fact")
-    displayEl.textContent = fact
-       
-       
-      })
-  }
-getFact()
-})
-
 // modal //
 
 var modal = document.getElementById("modal");
 var btn = document.getElementById("fun-fact-btn");
-var span = document.getElementsByClassName("close"[0]);
+var span = document.getElementsByClassName("close");
 
 // modal opens when button is clicked
 btn.onclick = function() {
@@ -45,28 +25,37 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
+
+
 let generate_btn = document.querySelector("#generate");
 let num_facts = document.querySelector("#num_facts");
 let animal_type = document.querySelector("#animal_type");
 
 generate_btn.addEventListener("click", function() {
-  let animal_value = animal_type.value.toLowerCase()
+  let animal_value = animal_type.value.toLowerCase();
+  //console.log(animal_value);
 
   if(parseInt(num_facts.value) > 5) {
     alert("Max is 5")
   }
-  function fetch() {
-    fetch('https://cat-fact.herokuapp.com/facts/random?animal_type=${animal_type}&amount=${num_facts}')
-    .then(response => response.text()) 
-    .then(data => {
-      let fact = JSON.parse(data).text
-      console.log(fact)
-      var displayEl = document.getElementById("result")
-      displayEl.textContent = fact
-
-    })
-  }
-
-  fetch()
-
+  var num_value = num_facts.value;
+  fetchCatApi(animal_value, num_value)
+  
 })
+
+function fetchCatApi(animal_value, num_value) {
+ //console.log(animal_value, num_facts);
+  fetch(`https://cat-fact.herokuapp.com/facts/random?animal_type=${animal_value}&amount=${num_value}`)
+  .then(response => response.json()) 
+  .then(data => {
+    //let fact = JSON.parse(data).text
+    console.log(data)
+    var displayEl = document.getElementById("result")
+    displayEl.textContent = data[0].text;
+    
+   
+    }
+  )}
+
+
+  
