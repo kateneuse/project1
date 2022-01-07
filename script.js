a5e97ff66fa7bc793b6730201aaf87ae92f20a55
Jacobcar <92916001+Jacobcar@users.noreply.github.com>
2022-01-06 20:13:51 -0800

    new key in script.js

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
-console.log ("hello")
+var apiKey = "a24b853d-07cb-41e6-92bc-bba4d171ec08"
 
-fetch("https://api.petfinder.com/v2/animals", {
+fetch("https://api.thecatapi.com/v1/images/search?limit=25",{
     method: 'GET',
-    mode: 'cors',
-    //credentials:"omit",
-    headers: {
-        // 'Content-type': 'application/json',
-        // 'Access-Control-Allow-Origin':"*",
-        'Authorization': "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJFSHFqOW9Fb3FhUFlaMFR2TkJhSXhaV2hEUnYwVnU2cVdLTTJ3U0tLU3JmYkxkUGF2dSIsImp0aSI6IjdhYjBhZWY0MzlkZDQ1MTdkYTUwN2ZjMjJmNzcyM2I2YjQ5MzIxZWQyZThhODRkZjFkMTljYjI0OWQyMTY4ZTgwNDJhM2FmMjQwMTZhMzYzIiwiaWF0IjoxNjQxMzM5MTMxLCJuYmYiOjE2NDEzMzkxMzEsImV4cCI6MTY0MTM0MjczMSwic3ViIjoiIiwic2NvcGVzIjpbXX0.AR3RE48hvHQepqH9gTX2s3iggsdBYZLRqNbDWleeJ1jy72xgfloREZBpBpiWx8jw5O2VTIBN4X1XwOmJwcqfAbD4e0iKikWiFwXQPztkIKipac9Q5CPbnUuF6Vj718AyvturMEvU4UIOxfD7DPsepjfjNN6GLfPkRoCShy_z8x7yiKJa5I-efBSR64DD4lwDx_dV6fk3xJY4hOtWs64EGUmr-zV7jHSD1pNnW0mhR05rkukWuLyrgghuJt9hUCpiRqNnW-LTM7pp7YBAjhAOT2LOXrDafMqfMFUjm1AEQBDPDnWGWtiBwqHY2ALZQSKImDdey45cL01nPN9rLbbH5A"
-
-    },
+    //mode: 'CORS',
+    headers: { 
+       // 'Authorization':"a24b853d-07cb-41e6-92bc-bba4d171ec08"
+},
 })
 
-.then(function(res){
-    return res.json()
-})
-.then(function(data){
+.then (response => response.json())
+.then (function(data){
     console.log(data)
 })
-.catch(function(err){
-    console.error(err)
-})
+ 
+//console.log ("hello")
+
+//fetch("https://api.petfinder.com/v2/animals", {
+    //method: 'GET',
+    //mode: 'cors',
+    //credentials:"omit",
+    //headers: {
+        // 'Content-type': 'application/json',
+        // 'Access-Control-Allow-Origin':"*",
+        //'Authorization': "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJFSHFqOW9Fb3FhUFlaMFR2TkJhSXhaV2hEUnYwVnU2cVdLTTJ3U0tLU3JmYkxkUGF2dSIsImp0aSI6IjdhYjBhZWY0MzlkZDQ1MTdkYTUwN2ZjMjJmNzcyM2I2YjQ5MzIxZWQyZThhODRkZjFkMTljYjI0OWQyMTY4ZTgwNDJhM2FmMjQwMTZhMzYzIiwiaWF0IjoxNjQxMzM5MTMxLCJuYmYiOjE2NDEzMzkxMzEsImV4cCI6MTY0MTM0MjczMSwic3ViIjoiIiwic2NvcGVzIjpbXX0.AR3RE48hvHQepqH9gTX2s3iggsdBYZLRqNbDWleeJ1jy72xgfloREZBpBpiWx8jw5O2VTIBN4X1XwOmJwcqfAbD4e0iKikWiFwXQPztkIKipac9Q5CPbnUuF6Vj718AyvturMEvU4UIOxfD7DPsepjfjNN6GLfPkRoCShy_z8x7yiKJa5I-efBSR64DD4lwDx_dV6fk3xJY4hOtWs64EGUmr-zV7jHSD1pNnW0mhR05rkukWuLyrgghuJt9hUCpiRqNnW-LTM7pp7YBAjhAOT2LOXrDafMqfMFUjm1AEQBDPDnWGWtiBwqHY2ALZQSKImDdey45cL01nPN9rLbbH5A"
+
+    //},
+//})
+
+//.then(function(res){
+  //  return res.json()
+//})
+//.then(function(data){
+//    console.log(data)
+//})
+//.catch(function(err){
+ //   console.error(err)
+//})
