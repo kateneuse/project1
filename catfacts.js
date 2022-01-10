72eef35e2c7d9e9e5386ced2ad37da916386970a
Jacobcar <92916001+Jacobcar@users.noreply.github.com>
2022-01-10 09:26:12 -0800

    addign to branch

--- catfacts.js
+++ catfacts.js
@@ -14,26 +14,34 @@ fetch("https://cat-fact.herokuapp.com/facts", {
 
 var modal = document.getElementById("modal");
 var btn = document.getElementById("fun-fact-btn");
+var span = document.getElementById("close");
 
 // modal opens when button is clicked
 btn.onclick = function () {
   modal.style.display = "block";
 }
+// modal closes when span (x) is clicked
+span.onclick = function () {
+  modal.style.display = "none";
+  var displayEl = document.getElementById("result")
+  displayEl.textContent = ''
+}
 
 
 
 
-let generate_btn = document.querySelector("#generate");
+let generate_btn = document.querySelector("#fun-fact-btn");
 let num_facts = document.querySelector("#num_facts");
 let animal_type = document.querySelector("#animal_type");
-let close = document.querySelector("#close");
+let closeEl = document.querySelector("#close");
 const clearEl = document.getElementById("clear-history")
-let searchHistory = JSON.parse(localStorage.getItem("generate")) || [];
+const historyEl = document.getElementById("history");
+let searchHistory = JSON.parse(localStorage.getItem("fun-fact-btn")) || [];
 
 
 generate_btn.addEventListener("click", function () {
   let animal_value = animal_type.value.toLowerCase();
-  //console.log(animal_value);
+  console.log(animal_value);
 
   if (parseInt(num_facts.value) > 5) {
     alert("Max is 5")
@@ -41,12 +49,19 @@ generate_btn.addEventListener("click", function () {
   var num_value = num_facts.value;
   fetchCatApi(animal_value, num_value)
 
-
-  
-
+  // save to local storage
+  const generateFact = num_facts.value + " " + animal_type.value + " facts";
+  console.log(generateFact)
+  searchHistory.push(generateFact);
+  localStorage.setItem("generate", JSON.stringify(searchHistory));
+  renderSearchHistory();
 })
-close.onclick = function (){
+
+closeEl.onclick = function (){
   modal.style.display = "none";
+  var displayEl = document.getElementById("result")
+  displayEl.textContent = ''
+
 }
 function fetchCatApi(animal_value, num_value) {
   //console.log(animal_value, num_facts);
@@ -68,15 +83,41 @@ function fetchCatApi(animal_value, num_value) {
 }
 
 
-//local storage//
-generate_btn.addEventListener("click", function () {
-  const generateFact = num_facts.value + animal_type.value;
-  fetchCatApi(generateFact);
-  searchHistory.push(generateFact);
-  localStorage.setItem(generate, JSON.stringify(searchHistory));
-  renderSearchHistory();
 
+//local storage//
+// generate_btn.addEventListener("click", function () {
+//   const generateFact = num_facts.value + animal_type.value;
+//   console.log(generateFact)
+//   searchHistory.push(generateFact);
+//   localStorage.setItem(generate, JSON.stringify(searchHistory));
+//   renderSearchHistory();
+  
+
+// })
+
+
+//clear history button 
+clearEl.addEventListener("click", function () {
+  localStorage.clear();
+  searchHistory = [];
+  renderSearchHistory();
 })
 
-
-
+function renderSearchHistory() {
+  historyEl.innerHTML = "";
+  for (let i=0; i < searchHistory.length;i++) {
+    const historyItem = document.createElement("input");
+    historyItem.setAttribute("type", "text");
+    historyItem.setAttribute("readonly", true);
+    historyItem.setAttribute("class", "form-control d-block bg-white");
+    historyItem.setAttribute("value", searchHistory[i]);
+    historyItem.addEventListener("click", function () {
+        fetchCatApi(historyItem.value);
+    })
+    historyEl.append(historyItem);
+}
+}
+renderSearchHistory();
+    if (searchHistory.length > 0) {
+        fetchCatApi(searchHistory[searchHistory.length - 1]);
+    }
--- script.js
+++ script.js
@@ -1,41 +1,29 @@
-var apiKey = "a24b853d-07cb-41e6-92bc-bba4d171ec08"
-var catcomments = document.getElementById("catcomment")
-fetch("https://api.thecatapi.com/v1/images/search?limit=25",{
-    method: 'GET',
-    //mode: 'CORS',
-    headers: { 
-       // 'Authorization':"a24b853d-07cb-41e6-92bc-bba4d171ec08"
-},
+function getPic() {
+    fetch("https://api.thecatapi.com/v1/images/search?limit=25", {
+        method: 'GET',
+        //mode: 'CORS',
+        headers: {
+        },
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data); 
+            // console.log(data[0].url)
+            for (i = 0; i < 1; i++) {
+                var displayEl = document.getElementById('display')
+                information = data[i].url;
+                console.log(information);
+                // displayEl.textContent = information
+                imgEl = document.createElement('img');
+                displayEl.appendChild(imgEl);
+                imgEl.src = information
+            }
+        })
+
+}
+var btnEl = document.getElementById('btn')
+btnEl.addEventListener('click', function () {
+    getPic()
+    var displayEl = document.getElementById('display')
+    displayEl.textContent = ''
 })
-
-.then (response => response.json())
-.then (function(data){
-    console.log(data)
-})
-
-  localStorage.setItem("catcomments","catcomment")
-  localStorage.getItem("catcomments")
-
-//console.log ("hello")
-
-//fetch("https://api.petfinder.com/v2/animals", {
-    //method: 'GET',
-    //mode: 'cors',
-    //credentials:"omit",
-    //headers: {
-        // 'Content-type': 'application/json',
-        // 'Access-Control-Allow-Origin':"*",
-        //'Authorization': "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJFSHFqOW9Fb3FhUFlaMFR2TkJhSXhaV2hEUnYwVnU2cVdLTTJ3U0tLU3JmYkxkUGF2dSIsImp0aSI6IjdhYjBhZWY0MzlkZDQ1MTdkYTUwN2ZjMjJmNzcyM2I2YjQ5MzIxZWQyZThhODRkZjFkMTljYjI0OWQyMTY4ZTgwNDJhM2FmMjQwMTZhMzYzIiwiaWF0IjoxNjQxMzM5MTMxLCJuYmYiOjE2NDEzMzkxMzEsImV4cCI6MTY0MTM0MjczMSwic3ViIjoiIiwic2NvcGVzIjpbXX0.AR3RE48hvHQepqH9gTX2s3iggsdBYZLRqNbDWleeJ1jy72xgfloREZBpBpiWx8jw5O2VTIBN4X1XwOmJwcqfAbD4e0iKikWiFwXQPztkIKipac9Q5CPbnUuF6Vj718AyvturMEvU4UIOxfD7DPsepjfjNN6GLfPkRoCShy_z8x7yiKJa5I-efBSR64DD4lwDx_dV6fk3xJY4hOtWs64EGUmr-zV7jHSD1pNnW0mhR05rkukWuLyrgghuJt9hUCpiRqNnW-LTM7pp7YBAjhAOT2LOXrDafMqfMFUjm1AEQBDPDnWGWtiBwqHY2ALZQSKImDdey45cL01nPN9rLbbH5A"
-
-    //},
-//})
-
-//.then(function(res){
-  //  return res.json()
-//})
-//.then(function(data){
-//    console.log(data)
-//})
-//.catch(function(err){
- //   console.error(err)
-//})
